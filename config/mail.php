0147fc28bea190e1ad41563e7b4de1b321bea0dc
hirad990 <iamhirad17@gmail.com>
2026-09-12 22:54:34 +0330

    feat: add mail server config

---
+++ config/mail.php
@@ -0,0 +1,2 @@
+<?php
+return ['smtp_host'=>getenv('SMTP_HOST') ?: 'mail.binercraft.ir','smtp_port'=>(int)(getenv('SMTP_PORT') ?: 465),'smtp_secure'=>getenv('SMTP_SECURE') ?: 'ssl','imap_host'=>getenv('IMAP_HOST') ?: 'mail.binercraft.ir','imap_port'=>(int)(getenv('IMAP_PORT') ?: 993),'imap_encryption'=>getenv('IMAP_ENCRYPTION') ?: 'ssl','imap_validate_cert'=>filter_var(getenv('IMAP_VALIDATE_CERT') ?: 'false',FILTER_VALIDATE_BOOLEAN)];
