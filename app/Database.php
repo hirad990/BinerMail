dc776498b172716bd19fdbbd46b2c8d8b0b29312
hirad990 <iamhirad17@gmail.com>
2026-09-12 22:54:45 +0330

    feat: add database layer

---
+++ app/Database.php
@@ -0,0 +1,2 @@
+<?php namespace BinerMail;
+class Database{private static ?\PDO $pdo=null;public static function pdo():\PDO{if(self::$pdo)return self::$pdo;$c=require __DIR__.'/../config/database.php';self::$pdo=new \PDO("mysql:host={$c['host']};port={$c['port']};dbname={$c['name']};charset=utf8mb4",$c['user'],$c['password'],[\PDO::ATTR_ERRMODE=>\PDO::ERRMODE_EXCEPTION,\PDO::ATTR_DEFAULT_FETCH_MODE=>\PDO::FETCH_ASSOC,\PDO::ATTR_EMULATE_PREPARES=>false]);return self::$pdo;}}
